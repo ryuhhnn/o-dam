bf35b649bf7d4b243020845046d6c00772e78c44
Ryan Coleman <me@ryuhhnn.com>
2019-12-23 20:58:53 -0600

    adds in full neo4j support for querying collections

--- src/model/collection.rs
+++ src/model/collection.rs
@@ -1,43 +1,5 @@
-pub trait Collection {
-  fn id(&self) -> &str;
-  fn uuid(&self) -> &str;
-  fn name(&self) -> &str;
-  fn description(&self) -> &Option<String>;
-  fn as_collection(&self) -> &dyn Collection;
-}
-
-pub struct CollectionData {
-  id: String,
-  uuid: String,
-  name: String,
-  description: Option<String>,
-}
-
-impl Collection for CollectionData {
-  fn id(&self) -> &str {
-    &self.id
-  }
-  fn uuid(&self) -> &str {
-    &self.uuid
-  }
-  fn name(&self) -> &str {
-    &self.name
-  }
-  fn description(&self) -> &Option<String> {
-    &self.description
-  }
-  fn as_collection(&self) -> &dyn Collection {
-    self
-  }
-}
-
-impl CollectionData {
-  pub fn new(id: &str, uuid: &str, name: &str, description: Option<&str>) -> CollectionData {
-    CollectionData {
-      id: id.to_owned(),
-      uuid: uuid.to_owned(),
-      name: name.to_owned(),
-      description: description.map(|d| d.to_owned()),
-    }
-  }
+pub struct Collection {
+  pub uuid: String,
+  pub name: String,
+  pub description: Option<String>,
 }
--- src/schema/schema.rs
+++ src/schema/schema.rs
@@ -1,4 +1,8 @@
+use crate::model::collection::Collection;
 use crate::neo::NeoStore;
+use juniper::FieldResult;
+use rocket_contrib::databases::rusted_cypher::Statement;
+use uuid::Uuid;
 
 pub struct Context {
   pub connection: NeoStore,
@@ -6,14 +10,41 @@ pub struct Context {
 
 impl juniper::Context for Context {}
 
+graphql_object!(Collection: () |&self| {
+  description: "Collection"
+
+  field uuid() -> &str as "The unique ID of the collection" {
+    &self.uuid
+  }
+
+  field name() -> &str as "Name of the collection" {
+    &self.name
+  }
+
+  field description() -> Option<String> as "Description of the collection" {
+    self.description.to_owned()
+  }
+});
+
 pub struct QueryRoot;
 
 graphql_object!(QueryRoot: Context as "Query" |&self| {
   description: "The root query object of the schema"
 
-  // field collection(id: String as "id of the collection") -> Option<&dyn Collection> {
-  //     self.get_collection(&id)
-  // }
+  field collections(&executor) -> FieldResult<Vec<Collection>> {
+    let mut results = Vec::new();
+    let statement = Statement::new("MATCH (c:Collection) RETURN c.uuid, c.name, c.description");
+    let query = executor.context().connection.exec(statement)?;
+
+    for result in query.rows() {
+      let uuid = result.get("c.uuid")?;
+      let name = result.get("c.name")?;
+      let description = result.get("c.description")?;
+      results.push(Collection { uuid: uuid, name: name, description: description})
+    }
+
+    Ok(results)
+  }
 });
 
 pub struct MutationRoot;
@@ -21,7 +52,22 @@ pub struct MutationRoot;
 graphql_object!(MutationRoot: Context | &self | {
   description: "The root mutation object of the schema"
 
-  // field collection(id: String as "id of the collection") -> Option<&dyn Collection> {
-  //     self.get_collection(&id)
-  // }
+  field collection(
+    &executor,
+    name: String as "id of the collection",
+    description: Option<String> as "description of the collection"
+  ) -> FieldResult<Collection> {
+    let uuid = Uuid::new_v4();
+    let statement = Statement::new(
+        "CREATE (c:Collection {uuid: {uuid}, name: {name}, description: {description}})"
+      );
+      // .with_param("uuid", uuid).unwrap()
+      // .with_param("name", name).unwrap()
+      // .with_param("description", description).unwrap();
+    let result = executor.context().connection.exec(statement).unwrap();
+
+    result.rows();
+
+    Ok(Collection { uuid: String::from("asdf"), name: String::from("asdf"), description: Some(String::from("asdf"))})
+  }
 });
