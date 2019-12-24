eaa1eb96191bc0b1618a2a2dcfe2d07e80a02a8b
Ryan Coleman <me@ryuhhnn.com>
2019-12-23 22:44:47 -0600

    adding in scaffold for frontend

---
+++ src/frontend/src/app.ts
@@ -0,0 +1,10 @@
+import { LitElement, TemplateResult, customElement, html } from "lit-element";
+
+@customElement("o-dam")
+export default class ODam extends LitElement {
+  render(): TemplateResult {
+    return html`
+      <h1>o-dam</h1>
+    `;
+  }
+}
